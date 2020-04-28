d40ce3d92098fe9f024d2428320fed9daf2c8172
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-28 19:41:32 +0300

    Stockslisting view and clean up layout

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -1,4 +1,5 @@
 class StocksController < ApplicationController
+
   def search
     if params[:stock].present?
       @stock = Stock.new_lookup(params[:stock])
@@ -19,4 +20,5 @@ class StocksController < ApplicationController
       end
     end
   end
+
 end
