241bdb373a5ab1c1f055c680b480f9a0af70c396
Alexiou Konstantinos <alexiou_kon@yahoo.gr>
2020-04-30 17:05:38 +0300

    Add User-Friend assocciation and friends listing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   has_many :user_stocks
   has_many :stocks, through: :user_stocks
+  has_many :friendships
+  has_many :friends, through: :friendships
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -25,4 +27,5 @@ class User < ApplicationRecord
     return "#{first_name} #{last_name}" if first_name || last_name
     "Anonymus"
   end
+
 end
